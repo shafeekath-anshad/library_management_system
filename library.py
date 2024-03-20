bafea8668d4befb42ccf1996a3ea7d349881f380
shafeekath anshad <118720882+shafeekath-anshad@users.noreply.github.com>
2024-03-20 19:21:12 +0530

    library_management_system.py

--- library.py
+++ library.py
@@ -12,15 +12,22 @@ class Members:
         self.return_date = return_date
 
 
-def result():
-    print("The book", b_name, "written by", authr, "is taken by", name, "on", borrow_date, "and return it on", return_date)
+def result(b_name, authr, name, borrow_date, return_date):
+    print("The book", b_name, "written by", authr, "is taken by", name, "on", borrow_date, "and return it on",
+          return_date)
 
 
-class Transactions(Members, Book):
-    pass
+class Transactions:
+    def __init__(self, member, book):
+        self.member = member
+        self.book = book
 
 
 book_obj = Book("oliver twist", "charles dicken")
 mem_obj = Members("anu", "india", "02/feb/2022", "12/feb/2022")
 t_obj = Transactions(mem_obj, book_obj)
-result()
+result(t_obj.book.book_name,
+       t_obj.book.author,
+       t_obj.member.name,
+       t_obj.member.borrow_date,
+       t_obj.member.return_date)
